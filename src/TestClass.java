c1cf887231b22bb23c6194074c16ab9456ef96f6
Jonathan <Jonathan@Jonathans-dator.lan>
2018-09-16 20:00:04 +0200

    Commit1

---
+++ src/TestClass.java
@@ -0,0 +1,20 @@
+
+public class TestClass {
+
+	public static int testIntOrg(int a) {
+		return a * 2;
+	}
+	
+	public static void testStringOrg(int a) {
+		int x = 0;
+		String printNrs = "";
+		while(x < a) {
+			printNrs = printNrs + x;
+			if(x==(a-1)==false) {
+				printNrs = printNrs + " ";
+			}
+			x++;
+		}
+	}
+	
+}
---
+++ src/TestMain.java
@@ -0,0 +1,12 @@
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		
+		int testA = 10;
+		testA = TestClass.testIntOrg(testA);
+		System.out.println(testA);
+
+	}
+
+}
